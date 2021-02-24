8d0168f1d2b833e80e6b597964332292da8df3d2
alyapin <alekseilyapin@mail.ru>
2021-02-25 00:37:02 +0300

    HW1_v1.3 add exit for more safe code

--- composers_info.py
+++ composers_info.py
@@ -5,19 +5,20 @@ if __name__ == '__main__':
         number = int(input("Type number of composers:"))
     except ValueError as err:
         print("This is not number, try again")
+        exit(1)
     composers = []
     for i in range(number):
         info = input(f'Type full name, year of birth and death of composer number {i + 1}:')
         info = ' '.join([item for item in info.split(" ") if item != ''])
         if len(info.split(" ")) != 4:
             print('Number of parameters is not 4')
-            # print(info.split(" "))
             break
         try:
             birth = int(info.split(" ")[2])
             death = int(info.split(" ")[3])
         except ValueError as err:
             print("This is not year, try again")
+            exit(1)
         composers.append(info)
     avg = []
     for i in range(number):
