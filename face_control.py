98db5b810b81fc40f77d255010b725e11fb89700
alyapin <alekseilyapin@mail.ru>
2021-02-25 16:08:12 +0300

    class participation

---
+++ face_control.py
@@ -0,0 +1,30 @@
+
+if __name__ == '__main__':
+    names = []
+    ages = []
+    while True:
+        info = input("Write your name and age please: ")
+        info = ' '.join([item for item in info.split(" ") if item != ''])
+        if len(info.split(" ")) != 2:
+            print("Number of parameters should be 2")
+            continue
+        try:
+            age = int(info.split(" ")[1])
+        except ValueError as err:
+            print("Second parametr is not age")
+            exit(1)
+        names.append(info.split(" ")[0])
+        ages.append(age)
+        answ = input("Do you want to continue ?(Y/n): ")
+        if answ in ['y', 'Y', 'yes', 'Yes']:
+            continue
+        elif answ in ['n', 'N', 'No', 'no']:
+            break
+        else:
+            print("I didn't get it, so continue") 
+
+    avg = []
+    for age, name in zip(ages, names):
+        print(f'Name is {name}, age is {age}')
+        avg.append(age)
+    print(f'Average age is {avg // len(ages)}')
